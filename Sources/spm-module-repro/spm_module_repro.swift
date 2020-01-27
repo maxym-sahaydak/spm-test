be40503da4f2afe3442e588d50ff2f8dd040209b
Maxym Sahaydak <maxym.sahaydak@indeema.com>
2020-01-27 11:56:26 +0200

    - added public init

--- Sources/spm-module-repro/spm_module_repro.swift
+++ Sources/spm-module-repro/spm_module_repro.swift
@@ -2,17 +2,22 @@ import ObjCFoo
 import ObjCBar
 
 public struct spm_module_repro {
-    
-  public func hello() {
-    // Use the class from ObjCFoo.
-    let foo = RWFoo()
-    let a = RWFoo()
-    print("Magic number: \(foo.magicNumber())")
 
-    // Use the class from ObjCBar (which uses ObjCFoo).
-    let bar = RWBar()
-    print("Number of popsicles: \(bar.numberOfPopsicles())")
-  }
+    public init() {
+        print("\(#function) - \(#file)")
+    }
+    
+    public func hello() {
+        // Use the class from ObjCFoo.
+        let foo = RWFoo()
+        let a = RWFoo()
+        print("Magic number: \(foo.magicNumber())")
+
+        // Use the class from ObjCBar (which uses ObjCFoo).
+        let bar = RWBar()
+        print("Number of popsicles: \(bar.numberOfPopsicles())")
+    }
+
 
 
 
